17be2f985e38065f0ff8505341004a9f12bb7083
vk2009 <67607214+vk2009@users.noreply.github.com>
2025-02-01 12:09:41 -0800

    run object detection code

---
+++ ObjectDetectionPT.py
@@ -0,0 +1,64 @@
+from ultralytics import YOLO
+import numpy as np
+import cv2
+
+# Load your model
+# home = '/Users/viditkwatra/Robotics_Antares_Vision_2025/AlgaeDetection/AlgaeDetectionModel/'
+# model = YOLO(f'{home}algaeDetectionBest.pt')
+model = YOLO("/path/model.pt")
+
+# Access the webcam
+cap = cv2.VideoCapture(0) 
+
+# FOR RECORDING VIDEO
+# Get the default frame width and height
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# Define the codec and create VideoWriter object
+# fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+# out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
+
+def plot_bboxes(results):
+    img = results[0].orig_img # original image
+    names = results[0].names # class names dict
+    scores = results[0].boxes.conf.numpy() # probabilities
+    classes = results[0].boxes.cls.numpy() # predicted classes
+    boxes = results[0].boxes.xyxy.numpy().astype(np.int32) # bboxes
+    for score, cls, bbox in zip(scores, classes, boxes): # loop over all bboxes
+        class_label = names[cls] # class name
+        label = f"{class_label} : {score:0.2f}" # bbox label
+        lbl_margin = 3 #label margin
+        img = cv2.rectangle(img, (bbox[0], bbox[1]),
+                            (bbox[2], bbox[3]),
+                            color=(0, 0, 255),
+                            thickness=2)
+        label_size = cv2.getTextSize(label, # labelsize in pixels 
+                                     fontFace=cv2.FONT_HERSHEY_SIMPLEX, 
+                                     fontScale=1, thickness=1)
+        lbl_w, lbl_h = label_size[0] # label w and h
+        lbl_w += 2* lbl_margin # add margins on both sides
+        lbl_h += 2*lbl_margin
+        img = cv2.rectangle(img, (bbox[0], bbox[1]), # plot label background
+                             (bbox[0]+lbl_w, bbox[1]-lbl_h),
+                             color=(0, 0, 255), 
+                             thickness=-1) # thickness=-1 means filled rectangle
+        cv2.putText(img, label, (bbox[0]+ lbl_margin, bbox[1]-lbl_margin), # write label to the image
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=1.0, color=(255, 255, 255),
+                    thickness=1)
+    return img
+
+while True:
+    ret, frame = cap.read()
+#     # Preprocess the frame
+    results = model(frame, verbose=False, conf=0.6) # run inference
+    img = plot_bboxes(results) # plot annotated bboxes
+    # out.write(img)
+    cv2.imshow("imgName", img) # show annotated image
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
