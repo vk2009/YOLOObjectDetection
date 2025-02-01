4155f761bf115fe72437587775a1b400a1c5c8d5
vk2009 <67607214+vk2009@users.noreply.github.com>
2025-02-01 12:14:25 -0800

    convertTFile.py also runs results

--- convertTFile.py
+++ convertTFile.py
@@ -1,16 +1,10 @@
-# from ultralytics import YOLO
-# # model: /Users/viditkwatra/Robotics_Antares_Vision_2025/AlgaeDetection/AlgaeDetectionModel/algaeDetectionBest.pt
-# # Load your PyTorch model
-# model = YOLO('/Users/viditkwatra/Robotics_Antares_Vision_2025/AlgaeDetection/AlgaeDetectionModel/algaeDetectionBest.pt')
-# model.export(format="tflite", int8=True)
-
 from ultralytics import YOLO
 
-# # Load the YOLO11 model
-# model = YOLO("/Users/viditkwatra/Robotics_Antares_Vision_2025/AlgaeDetection/AlgaeDetectionModel/algaeDetectionBest.pt")
+# Load your PyTorch model
+model = YOLO('/path/model.pt')
 
-# # Export the model to TFLite format
-# model.export(format="tflite")  # creates 'yolo11n_float32.tflite'
+# Export the model to TFLite format
+model.export(format="tflite", int8=True) # int8 can be set to false
 
 # Load the exported TFLite model
 tflite_model = YOLO("algaeDetectionBest_float16.tflite")
